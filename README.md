0b9cfc1ab77dcf3e28bcd82f90e0c8ae7979952b
Jan(U117292) <jan.kuonen.93@gmail.com>
2018-07-23 23:59:13 +0200

    add webpack & bable

--- README.md
+++ README.md
@@ -1,44 +1,46 @@
 # MAGmapper
-A Videomapper based on three.js<br />
-Should run on any plattform using Chrome or Firefox.<br />
+## Basic Idea
+A Videomapper based on three.js
+Should run on any plattform using Chrome or Firefox.
 
-Maybe we should use this: https://github.com/glowbox/maptasticjs <br />
-Looks great and has the basic functions.<br />
-Now we have to figure out if it is possible to load a video with omxplayer within a website and this tool,<br />
-so we could gain fully HW support on RPi and could use the sync script to sync multiple players.<br />
-These guys are talking about omxplayer supprt: https://www.raspberrypi.org/forums/viewtopic.php?t=40860
-but have no idea if this supports three.js and distorsion. Try implementation with this: <iframe src="file:///homepage.html?cmd=omxplayer%20--win%200,0,1920,1080%20/home/pi/video.mp4" width="2px" height="2px" frameborder="0"></iframe>
-<br />
-If there is no way to gain Video HW support thru iframe and omxplayer, we have to switch to this https://forums.resin.io/t/fullscreen-webkit-browser-with-hw-accelerated-css-webgl-and-html5-video-on-the-raspberrypi-3/741 operation system for full WebGL HW support.
+Maybe we should use [maptasticjs](https://github.com/glowbox/maptasticjs).
+Looks great and has the basic functions.
+
+Now we have to figure out if it is possible to load a video with omxplayer within a website and this tool,
+so we could gain fully HW support on RPi and could use the sync script to sync multiple players.
+
+These guys are talking about [omxplayer support](https://www.raspberrypi.org/forums/viewtopic.php?t=40860)
+but have no idea if this supports three.js and distorsion. Try implementation with this:
+
+<iframe src="file:///homepage.html?cmd=omxplayer%20--win%200,0,1920,1080%20/home/pi/video.mp4" width="2px" height="2px" frameborder="0"></iframe>
+
+If there is no way to gain Video HW support thru iframe and omxplayer, we have to switch to [this](https://forums.resin.io/t/fullscreen-webkit-browser-with-hw-accelerated-css-webgl-and-html5-video-on-the-raspberrypi-3/741) operation system for full WebGL HW support.
 
 
-These guys do map with three.js : http://www.floz.fr/Tsuki8Projection-mapping-in-Japan <br />
+These guys do [map](http://www.floz.fr/Tsuki8Projection-mapping-in-Japan) with three.js. 
 
-Testimage: https://pocketvj.com/video/PVJ_Testscreen.png <br />
-Testvideo: https://pocketvj.com/video/PVJ_Testvideo2018.mp4 <br />
-<br /><br />
-Its in the very early developement stage, reached so far: <br />
+- [Testimage](https://pocketvj.com/video/PVJ_Testscreen.png)
+- [Testvideo](https://pocketvj.com/video/PVJ_Testvideo2018.mp4)
+
+
+Its in the very early developement stage, reached so far:
 
 - Basic understandment of Three.js structure.
 - Draggable 3d object in browser.
 
-<br />
+## Next Steps:
 
-
-### Next Steps:
-<br />
-- Change BoxGeometry to BufferGeometryquad style surface like in the example here: https://threejs.org/examples/#webgl_buffergeometry_indexed
+- Change BoxGeometry to [BufferGeometryquad](https://threejs.org/examples/#webgl_buffergeometry_indexed) style surface like in the example here:
 - Make corners dragable, so when mouse + click on a corner, you can move its position
-- Add circular markers to corners to see them better, something like the red dot here: https://threejs.org/examples/#webgl_interactive_lines
-- Load custom Textures (images, image folders, videos) https://threejs.org/docs/#api/textures/VideoTexture
-https://stackoverflow.com/questions/37884013/adding-video-as-texture-in-three-js
-- Test spline planes, is it possible to use bezier handles on them?: https://threejs.org/examples/#webgl_geometry_nurbs
-<br />
-<br />
+- Add circular markers to corners to see them better, something like the red dot [here](https://threejs.org/examples/#webgl_interactive_lines)
+- Load custom [Textures](https://threejs.org/docs/#api/textures/VideoTexture) (images, image folders, videos)
+- Adding a [video](https://stackoverflow.com/questions/37884013/adding-video-as-texture-in-three-js) to a texture
+- Test spline planes, is it possible to use bezier handles on [webgl_geometry_nurbs](https://threejs.org/examples/#webgl_geometry_nurbs)
 
+## Milestones
 ### Milestone1:
 - Functional prototype of a quad surface with a video texture which can be distorted with dragging the corners. Corners get higlighted when mouse is near them (if possible).
-<br />
+
 
 ### Milestone2:
 - Add keyboard shortcuts (or overlay menu) to step thru video/image sources (changing sources).
@@ -66,4 +68,20 @@ https://stackoverflow.com/questions/37884013/adding-video-as-texture-in-three-js
 - Sync various players with each other, or source from tcp-server.
 
 
+## Usage
+
+### Install
+```sh
+$ npm install
+```
+
+### Serve
+```sh
+$ npm start
+```
+
+### Build
+```sh
+$ npm run build
+```
 
--- src/main.js
+++ src/main.js
@@ -1,200 +1,204 @@
-			var camera, scene, renderer, stats;
-			var mesh;
+
+
+
+var camera, scene, renderer, stats;
+var mesh;
 // hier ist das hauptprogramm
 //
-			init();
-			animate();
+init();
+animate();
 
-			function init() {
+function init() {
 
-				//hier wird die standard 3d scene generiert/geladen
+    //hier wird die standard 3d scene generiert/geladen
 
-				camera = new THREE.PerspectiveCamera( 27, window.innerWidth / window.innerHeight, 1, 3500 );
-				camera.position.z = 64;
+    camera = new THREE.PerspectiveCamera(27, window.innerWidth / window.innerHeight, 1, 3500);
+    camera.position.z = 64;
 
-				scene = new THREE.Scene();
-				scene.background = new THREE.Color( 0x050505 );
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x050505);
 
-				//
-				//generates the lights, but we need to get rid of this, maybe change material type
-				//
+    //
+    //generates the lights, but we need to get rid of this, maybe change material type
+    //
 
-				var ambientLight = new THREE.AmbientLight( 0xffffff );
-				scene.add( ambientLight );
-				
-
-				//Generates the object, in this case a buffer geometry plane
-
-				var geometry = new THREE.BufferGeometry();
-
-				var indices = [];
-
-				var vertices = [];
-				var normals = [];
-				var colors = [];
-
-				var size = 20;
-				var segments = 10;
-
-				var halfSize = size / 2;
-				var segmentSize = size / segments;
+    var ambientLight = new THREE.AmbientLight(0xffffff);
+    scene.add(ambientLight);
 
 
-				//here we add the controls to drag the geo, the thing in brackets is the name of the object?, 
-				//i think it is not working because there is no cube object, how is the name of out object?:
-				// or should the var cube = define the new THREE.BufferGeo?
+    //Generates the object, in this case a buffer geometry plane
 
-				var cube = new THREE.Mesh( geometry);
-				var controls = prepareControls(cube);
+    var geometry = new THREE.BufferGeometry();
 
-				// generate vertices, normals and color data for a simple grid geometry
+    var indices = [];
 
-				for ( var i = 0; i <= segments; i ++ ) {
+    var vertices = [];
+    var normals = [];
+    var colors = [];
 
-					var y = ( i * segmentSize ) - halfSize;
+    var size = 20;
+    var segments = 10;
 
-					for ( var j = 0; j <= segments; j ++ ) {
+    var halfSize = size / 2;
+    var segmentSize = size / segments;
 
-						var x = ( j * segmentSize ) - halfSize;
 
-						vertices.push( x, - y, 0 );
-						normals.push( 0, 0, 1 );
+    //here we add the controls to drag the geo, the thing in brackets is the name of the object?,
+    //i think it is not working because there is no cube object, how is the name of out object?:
+    // or should the var cube = define the new THREE.BufferGeo?
 
-						var r = ( x / size ) + 0.5;
-						var g = ( y / size ) + 0.5;
+    var cube = new THREE.Mesh(geometry);
+    var controls = prepareControls(cube);
 
-						colors.push( r, g, 1 );
+    // generate vertices, normals and color data for a simple grid geometry
 
-					}
+    for (var i = 0; i <= segments; i++) {
 
-				}
+        var y = (i * segmentSize) - halfSize;
 
-				// generate indices (data for element array buffer)
+        for (var j = 0; j <= segments; j++) {
 
-				for ( var i = 0; i < segments; i ++ ) {
+            var x = (j * segmentSize) - halfSize;
 
-					for ( var j = 0; j < segments; j ++ ) {
+            vertices.push(x, -y, 0);
+            normals.push(0, 0, 1);
 
-						var a = i * ( segments + 1 ) + ( j + 1 );
-						var b = i * ( segments + 1 ) + j;
-						var c = ( i + 1 ) * ( segments + 1 ) + j;
-						var d = ( i + 1 ) * ( segments + 1 ) + ( j + 1 );
+            var r = (x / size) + 0.5;
+            var g = (y / size) + 0.5;
 
-						// generate two faces (triangles) per iteration
+            colors.push(r, g, 1);
 
-						indices.push( a, b, d ); // face one
-						indices.push( b, c, d ); // face two
+        }
 
-					}
+    }
 
-				}
+    // generate indices (data for element array buffer)
 
-				//
+    for (var i = 0; i < segments; i++) {
 
-				geometry.setIndex( indices );
-				geometry.addAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
-				geometry.addAttribute( 'normal', new THREE.Float32BufferAttribute( normals, 3 ) );
-				geometry.addAttribute( 'color', new THREE.Float32BufferAttribute( colors, 3 ) );
+        for (var j = 0; j < segments; j++) {
 
-				var material = new THREE.MeshPhongMaterial( {
-					specular: 0x111111, shininess: 250,
-					side: THREE.DoubleSide, vertexColors: THREE.VertexColors
-				} );
+            var a = i * (segments + 1) + (j + 1);
+            var b = i * (segments + 1) + j;
+            var c = (i + 1) * (segments + 1) + j;
+            var d = (i + 1) * (segments + 1) + (j + 1);
 
-				mesh = new THREE.Mesh( geometry, material );
-				scene.add( mesh );
+            // generate two faces (triangles) per iteration
 
-				//
+            indices.push(a, b, d); // face one
+            indices.push(b, c, d); // face two
 
-				renderer = new THREE.WebGLRenderer( { antialias: true } );
-				renderer.setPixelRatio( window.devicePixelRatio );
-				renderer.setSize( window.innerWidth, window.innerHeight );
-				document.body.appendChild( renderer.domElement );
+        }
 
-				//
+    }
 
-				stats = new Stats();
-				document.body.appendChild( stats.dom );
+    //
 
-				//
+    geometry.setIndex(indices);
+    geometry.addAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+    geometry.addAttribute('normal', new THREE.Float32BufferAttribute(normals, 3));
+    geometry.addAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
 
-				var gui = new dat.GUI()
-				gui.add( material, 'wireframe' );
+    var material = new THREE.MeshPhongMaterial({
+        specular: 0x111111, shininess: 250,
+        side: THREE.DoubleSide, vertexColors: THREE.VertexColors
+    });
 
-				//
+    mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
 
-				window.addEventListener( 'resize', onWindowResize, false );
+    //
 
-			}
+    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
-			function onWindowResize() {
+    //
 
-				camera.aspect = window.innerWidth / window.innerHeight;
-				camera.updateProjectionMatrix();
+    stats = new Stats();
+    document.body.appendChild(stats.dom);
 
-				renderer.setSize( window.innerWidth, window.innerHeight );
+    //
 
-			}
+    var gui = new dat.GUI()
+    gui.add(material, 'wireframe');
+
+    //
+
+    window.addEventListener('resize', onWindowResize, false);
+
+}
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+
 //some drag controls, have no idea, just copied it, and it does not work so far:
 // DragControls benötigt zwangsweise ein array von meshes
 
-			var dragControls = new THREE.DragControls(objects, camera, renderer.domElement);
+var dragControls = new THREE.DragControls(objects, camera, renderer.domElement);
 
-			dragControls.addEventListener( 'dragstart', function ( event ) { controls.enabled = false; } );
-			dragControls.addEventListener( 'dragend', function ( event ) { controls.enabled = true; } );
-
+dragControls.addEventListener('dragstart', function (event) {
+    controls.enabled = false;
+});
+dragControls.addEventListener('dragend', function (event) {
+    controls.enabled = true;
+});
 
 
 // here we add some drag controls:
 /**
-hier wird der controller für das drag and drop definiert.
-Dieser wird für die EventListener benötigt
-*/
-			function prepareControls(obj){
-				var controls = new THREE.TrackballControls(obj);
+ hier wird der controller für das drag and drop definiert.
+ Dieser wird für die EventListener benötigt
+ */
+function prepareControls(obj) {
+    var controls = new THREE.TrackballControls(obj);
 
-				controls.rotateSpeed = 1.0;
-				controls.zoomSpeed = 1.2;
-				controls.panSpeed = 0.8;
-				controls.noZoom = false;
-				controls.noPan = false;
-				controls.staticMoving = true;
-				controls.dynamicDampingFactor = 0.3;
+    controls.rotateSpeed = 1.0;
+    controls.zoomSpeed = 1.2;
+    controls.panSpeed = 0.8;
+    controls.noZoom = false;
+    controls.noPan = false;
+    controls.staticMoving = true;
+    controls.dynamicDampingFactor = 0.3;
 
-				return controls
-			}
-
-
+    return controls
+}
 
 
+//schnall ich noch nicht ganz was da abgeht
 
-			//schnall ich noch nicht ganz was da abgeht
-
-			function animate() {
-				requestAnimationFrame( animate );
-				stats.update();
-				renderer.render( scene, camera );
-			}
+function animate() {
+    requestAnimationFrame(animate);
+    stats.update();
+    renderer.render(scene, camera);
+}
 
 
 //render und animate sind im beispiel separat, weiss nicht wieso:
 
 
-			//function animate() {
+//function animate() {
 
-			//	requestAnimationFrame( animate );
-			//	render();
-			//	stats.update();
+//	requestAnimationFrame( animate );
+//	render();
+//	stats.update();
 
-			//}
+//}
 
-			//function render() {
+//function render() {
 //
-			//	var time = Date.now() * 0.001;
+//	var time = Date.now() * 0.001;
 
-			//	mesh.rotation.x = time * 0.25;
-			//	mesh.rotation.y = time * 0.5;
+//	mesh.rotation.x = time * 0.25;
+//	mesh.rotation.y = time * 0.5;
 
-			//	renderer.render( scene, camera );
+//	renderer.render( scene, camera );
 
-			//}
+//}
