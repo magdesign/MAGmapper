38c3a1b84cc6d6194b1810a82efdfd9c856bd07f
magdesign <magdesign@sandbox.ch>
2018-08-02 18:16:18 +0200

    add image bias

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ import {
     PerspectiveCamera,
     WebGLRenderer,
     PlaneGeometry,
+    Geometry,
+    BufferGeometry,
+    BufferAttribute,
     TextureLoader,
     MeshPhongMaterial,
     LoadingManager,
@@ -10,6 +13,8 @@ import {
     Mesh,
     DoubleSide,
     Vector2,
+    Vector3,
+    Face3,
     ShapeBufferGeometry,
     Shape
 } from "three"
@@ -50,7 +55,7 @@ function animate() {
     renderer.render(scene, camera);
 }
 
-function createMapper() {
+function createMapperShapeBufferGeometry() {
 
     let triangleShape = new Shape();
     let startpoint = 0.5;
@@ -59,6 +64,9 @@ function createMapper() {
     triangleShape.lineTo(startpoint + 1, startpoint);
     triangleShape.lineTo(startpoint, startpoint - 1); // close path
     triangleShape.lineTo(startpoint-1, startpoint); // close path
+
+
+
     
     let geometry = new ShapeBufferGeometry(triangleShape);
 
@@ -66,14 +74,107 @@ function createMapper() {
     manager.onProgress = function ( item, loaded, total ) {
         console.log( item, loaded, total );
     };
-    var texture = new TextureLoader(manager).load( 'textures/UV_Grid_Sm.jpg' );
-
-
+    let texture = new TextureLoader(manager).load( 'textures/UV_Grid_Sm.jpg' );
     let material = new MeshBasicMaterial({map: texture});
 
 
     return new Mesh(geometry, material);
 }
+
+function createMapper2() {
+
+    let geometry = new BufferGeometry();
+
+    var vertices = new Float32Array( [
+        -1.0, -1.0,  0,
+         1.0, -1.0,  0,
+         1.0,  1.0,  0,
+
+         1.0,  1.0,  0,
+        -1.0,  1.0,  0,
+        -1.0, -1.0,  0
+    ] );
+    
+    //geometry.setIndex([0,1,2,3])
+
+    var uvs = new Float32Array([
+        -1.0, -1.0,
+        1.0, -1.0,
+        -1.0, 1.0,
+        1.0, 1.0,
+      ])
+    geometry.addAttribute( 'position', new BufferAttribute( vertices, 3 ));
+   // geometry.addAttribute( 'uv', new BufferAttribute( uvs, 2 ) );
+    //var material = new MeshBasicMaterial( { color: 0xff0000 } );
+    var manager = new LoadingManager();
+    manager.onProgress = function ( item, loaded, total ) {
+        console.log( item, loaded, total );
+    };
+    
+    let texture = new TextureLoader(manager).load( 'textures/UV_Grid_Sm.jpg' );
+    let material = new MeshBasicMaterial({map: texture});
+
+
+    return new Mesh(geometry, material);
+}
+
+
+function createMapper() {
+
+
+    var imageSize = {
+        width: 1, 
+        height: 1.5
+    };
+
+    // Identify the x, y, z coords where the image should be placed
+    var coords = {
+        x: -1, 
+        y: -1.75, 
+        z: 0
+    };
+
+    var bias = 1.5;
+
+
+    var vertices = new Float32Array([
+        coords.x, coords.y, coords.z, // bottom left
+        coords.x + imageSize.width, coords.y, coords.z, // bottom right
+        coords.x + imageSize.width + bias, coords.y + imageSize.height+ bias, coords.z, // upper right
+        coords.x, coords.y + imageSize.height, coords.z, // upper left
+    ])
+  
+    var uvs = new Float32Array([
+        0.0, 0.0,
+        1.0, 0.0,
+        1.0, 1.0,
+        0.0, 1.0,
+    ])
+  
+
+    let geometry = new BufferGeometry();
+    geometry.setIndex([0,1,2, 2,3,0])
+    geometry.addAttribute('position', new BufferAttribute( vertices, 3 ));
+    geometry.addAttribute('uv', new BufferAttribute( uvs, 2) )
+
+
+
+
+
+   // geometry.addAttribute( 'uv', new BufferAttribute( uvs, 2 ) );
+    //var material = new MeshBasicMaterial( { color: 0xff0000 } );
+    var manager = new LoadingManager();
+    manager.onProgress = function ( item, loaded, total ) {
+        console.log( item, loaded, total );
+    };
+    
+    let texture = new TextureLoader(manager).load( 'textures/UV_Grid_Sm.jpg' );
+    let material = new MeshBasicMaterial({map: texture});
+
+
+    return new Mesh(geometry, material);
+}
+
 
 
 function createCube(x, y, size, color) {
