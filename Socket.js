24157a7c202b24ce5db3dd42e1ed1c2ef6f475b1
Jaun1011 <jan.kuonen.93@gmail.com>
2018-08-27 22:11:45 +0200

    add logger for sockets.js

--- Socket.js
+++ Socket.js
@@ -18,23 +18,43 @@ console.log(
     "        \\/            \\/     \\/    \\/                                  \n" +
     "###########################################################################");
 
-console.log("listen on port: " + port);
+var log4js = require('log4js');
+var logger = log4js.getLogger();
+
+logger.level = 'debug';
 
 
+logger.info("Listen on port: " + port);
 
 wss.on('connection', function (w) {
-
-    console.log("> connection established");
+    w.uid = uuid();
+    logger.info("client registered: " + w.uid);
 
     w.on('message', function (msg) {
-        console.log(msg);
+        logger.debug(msg);
 
         wss.clients.forEach(function (client) {
+            logger.info("update client: ", client.uid);
+            logger.debug(msg);
+
             client.send(msg)
         })
     });
 
     w.on('close', function () {
-        console.log("< disconnection")
+        logger.info("removed client: " + w.uid);
     });
 });
+
+function uuid() {
+    var uuid = "", i, random;
+    for (i = 0; i < 32; i++) {
+        random = Math.random() * 16 | 0;
+
+        if (i == 8 || i == 12 || i == 16 || i == 20) {
+            uuid += "-"
+        }
+        uuid += (i == 12 ? 4 : (i == 16 ? (random & 3 | 8) : random)).toString(16);
+    }
+    return uuid;
+}
