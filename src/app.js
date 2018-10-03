5e0d314108c859827af5c5722ce84b07c75ed9cb
Jaun1011 <jan.kuonen.93@gmail.com>
2018-10-03 23:22:36 +0200

    add new button for wireframe enable and disable

--- src/app.js
+++ src/app.js
@@ -10,72 +10,81 @@ import {
     Mesh,
     VideoTexture,
     Sprite,
-    SpriteMaterial
+    SpriteMaterial,
+    LinearFilter,
+    ClampToEdgeWrapping
 } from "three"
 import DragControls from "three-dragcontrols"
 import {Mapper} from './mapper.js'
 import {Shift} from "./shift";
 
 
-let connection = new WebSocket("ws://localhost:9030");
+let config = {
+    websocketUrl: "ws://localhost:9030",
+    cameraPosition: 10,
+    wireframe: false,
+    size: 30,
+    length: 5
+};
 
+
+let connection = new WebSocket(config.websocketUrl);
 
 let scene = new Scene();
 let camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 10;
+camera.position.z = config.cameraPosition;
+
 
 let renderer = new WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 
+
+// Button Area
+document.getElementById("wireframe").addEventListener("click", () => {
+    config.wireframe = !config.wireframe;
+    renderMappingWithWebSocket();
+});
+
 document.body.appendChild(renderer.domElement);
-
-
 connection.onopen = () => prepareShapes(camera, scene);
 animate();
 
+function getEdgePoints(scene) {
+    return {
+        topRight: scene.children[4].position,
+        topLeft: scene.children[2].position,
+        bottomRight: scene.children[3].position,
+        bottomLeft: scene.children[1].position
+    };
+}
+
+function renderMappingWithWebSocket(){
+    const points = getEdgePoints(scene);
+    renderMapping(points);
+    connection.send(JSON.stringify({points, config}));
+}
+
 function prepareShapes(camera, scene) {
 
-    const size = 45;
-    const length = 5;
-
-    let mapper = createMapper(size, length);
+    let mapper = createMapper(config.size, config.length);
     mapper.scene.forEach(item => scene.add(item));
 
 
     let dragControls = new DragControls(mapper.handler, camera, renderer.domElement);
-    dragControls.addEventListener('dragend', () => {
-        const topRight = scene.children[4].position;
-        const topLeft = scene.children[2].position;
-
-        const bottomRight = scene.children[3].position;
-        const bottomLeft = scene.children[1].position;
-
-        let mapping = calcMapping(size, length);
-
-        let vert = Shift.shift(size, bottomLeft, topLeft, bottomRight, topRight);
-
-        let geometry = buildBufferGeometry(vert, mapping.uvs, mapping.indices);
-        let mapperMesh = buildVideoMesh(geometry);
-
-        scene.children[0] = mapperMesh;
-
-        renderer.render(scene, camera);
-        connection.send(JSON.stringify({topRight, topLeft, bottomRight, bottomLeft}));
+    dragControls.addEventListener('drag', () => {
+        renderMappingWithWebSocket();
     });
 
 
     connection.onmessage = e => {
+        const data = JSON.parse(e.data);
+        config  =  data.config;
 
-        const points = JSON.parse(e.data);
-        console.log(points);
+        const topRight = data.points.topRight;
+        const topLeft = data.points.topLeft;
 
-
-        const topRight = points.topRight;
-        const topLeft = points.topLeft;
-
-        const bottomRight = points.bottomRight;
-        const bottomLeft = points.bottomLeft;
-
+        const bottomRight = data.points.bottomRight;
+        const bottomLeft = data.points.bottomLeft;
 
         scene.children[4].position.x = topRight.x;
         scene.children[4].position.y = topRight.y;
@@ -89,17 +98,19 @@ function prepareShapes(camera, scene) {
         scene.children[1].position.x = bottomLeft.x;
         scene.children[1].position.y = bottomLeft.y;
 
-        let mapping = calcMapping(size, length);
-        let vert = Shift.shift(size, bottomLeft, topLeft, bottomRight, topRight);
-
-        let geometry = buildBufferGeometry(vert, mapping.uvs, mapping.indices);
-        let mapperMesh = buildVideoMesh(geometry);
-
-        scene.children[0] = mapperMesh;
-        renderer.render(scene, camera);
+        renderMapping(data.points);
     }
 }
 
+
+function renderMapping(points) {
+    let mapping = calcMapping(config.size, config.length);
+    let vert = Shift.shift(config.size, points.bottomLeft, points.topLeft, points.bottomRight, points.topRight);
+    let geometry = buildBufferGeometry(vert, mapping.uvs, mapping.indices);
+
+    scene.children[0] = buildVideoMesh(geometry);
+    renderer.render(scene, camera);
+}
 
 function animate() {
     requestAnimationFrame(animate);
@@ -108,7 +119,9 @@ function animate() {
 
 
 function makeSprite(x, y) {
-    const texture = new TextureLoader().load("textures/sprite0.png");
+    const texture = new TextureLoader()
+        .load("textures/sprite0.png");
+
     const material = new SpriteMaterial({map: texture});
 
     let sprite = new Sprite(material);
@@ -126,15 +139,6 @@ function buildBufferGeometry(vertices, uvs, indices) {
     return geometry;
 }
 
-function buildVideoMesh2(geometry) {
-    let manager = new LoadingManager();
-    manager.onProgress = function (item, loaded, total) {
-        console.log(item, loaded, total);
-    };
-    const texture = new TextureLoader(manager).load('textures/UV_Grid_Sm.jpg');
-    return new Mesh(geometry, new MeshBasicMaterial({map: texture, wireframe: false}))
-}
-
 //buildMesh
 function buildVideoMesh(geometry) {
     let manager = new LoadingManager();
@@ -142,10 +146,15 @@ function buildVideoMesh(geometry) {
         console.log(item, loaded, total);
     };
 
-    var video = document.getElementById('video');
+    let video = document.getElementById('video');
 
     const texture = new VideoTexture(video);
-    return new Mesh(geometry, new MeshBasicMaterial({map: texture, wireframe: false}))
+
+    texture.generateMipmaps = false;
+    texture.wrapS = texture.wrapT = ClampToEdgeWrapping;
+    texture.minFilter = LinearFilter;
+
+    return new Mesh(geometry, new MeshBasicMaterial({map: texture, wireframe: config.wireframe}))
 }
 
 
@@ -162,7 +171,10 @@ function createMapper(size, length) {
     let geometry = buildBufferGeometry(mapping.vertices, mapping.uvs, mapping.indices);
     let mapperMesh = buildVideoMesh(geometry);
 
-    const handler = Mapper.edges(mapping.vertices).map(vert => makeSprite(vert.x, vert.y));
+    const handler = Mapper
+        .edges(mapping.vertices)
+        .map(vert => makeSprite(vert.x, vert.y));
+
     const scene = Array.concat([mapperMesh], handler);
 
     return {
