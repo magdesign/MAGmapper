97cee88481bc077c1f437a1b4f365dd4d091433f
Jan(U117292) <jan.kuonen.93@gmail.com>
2018-10-15 21:26:31 +0200

    build...

--- src/videobar.js
+++ src/videobar.js
@@ -1,32 +1,36 @@
 import {Renderer, EventType} from "./renderer";
 
+
+function getVideoElement() {
+    return document.getElementById("video");
+}
+
 export function init(mapper, config) {
     // set length of video in slider
-    let video = document.getElementById("video");
 
     document.getElementById("video-slider").max = document.getElementById("video").duration;
 
     document.getElementById("video-stop")
         .addEventListener("click",
             () => {
-                video.pause();
+                getVideoElement().pause();
                 Renderer.renderMappingWithWebSocket(mapper, config, EventType.video);
             });
 
     document.getElementById("video-start")
         .addEventListener("click",
             () => {
-                video.play();
+                getVideoElement().play();
                 Renderer.renderMappingWithWebSocket(mapper, config, EventType.video);
             });
 
     document.getElementById("video").ontimeupdate =
         () => {
-            document.getElementById("video-slider").value = video.currentTime;
+            document.getElementById("video-slider").value = getVideoElement().currentTime;
         };
 
     document.getElementById("video-slider").oninput = () => {
-        video.currentTime = document.getElementById("video-slider").value;
+        getVideoElement().currentTime = document.getElementById("video-slider").value;
         Renderer.renderMappingWithWebSocket(mapper, config, EventType.video);
     }
 }
