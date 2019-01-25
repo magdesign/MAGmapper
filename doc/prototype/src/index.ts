52a381151d7c5d5283665b8dcce07f6811064238
jku <jku@localhost.localdomain>
2019-01-25 15:06:01 +0100

    add prototype in ts

---
+++ doc/prototype/src/index.ts
@@ -0,0 +1,64 @@
+import './style.css'
+import * as THREE from 'three'
+
+// create the scene
+let scene = new THREE.Scene()
+
+// create the camera
+let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+let renderer:THREE.WebGLRenderer = new THREE.WebGLRenderer()
+
+// set size
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+// add canvas to dom
+document.body.appendChild(renderer.domElement)
+
+// add axis to the scene
+let axis = new THREE.AxesHelper(10)
+
+scene.add(axis)
+
+// add lights
+let light = new THREE.DirectionalLight(0xffffff, 1.0)
+
+light.position.set(100, 100, 100)
+
+scene.add(light)
+
+let light2 = new THREE.DirectionalLight(0xffffff, 1.0)
+
+light2.position.set(-100, 100, -100)
+
+scene.add(light2)
+
+let material = new THREE.MeshBasicMaterial({
+	color: 0xaaaaaa,
+	wireframe: true
+})
+
+// create a box and add it to the scene
+let box = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), material)
+
+scene.add(box)
+
+box.position.x = 0.5
+box.rotation.y = 0.5
+
+camera.position.x = 5
+camera.position.y = 5
+camera.position.z = 5
+
+camera.lookAt(scene.position)
+
+function animate(): void {
+	requestAnimationFrame(animate)
+	render()
+}
+
+function render(): void {
+	renderer.render(scene, camera)
+}
+
+animate()
---
+++ doc/prototype/webpack.config.js
@@ -0,0 +1,75 @@
+/* Configure HTMLWebpack plugin */
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: __dirname + '/src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+})
+
+/* Configure BrowserSync */
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+const BrowserSyncPluginConfig = new BrowserSyncPlugin({
+    host: 'localhost',
+    port: 3000,
+    proxy: 'http://localhost:8080/'
+}, config = {
+    reload: false
+})
+
+/* Configure ProgressBar */
+const ProgressBarPlugin = require('progress-bar-webpack-plugin')
+const ProgressBarPluginConfig = new ProgressBarPlugin()
+
+/* Export configuration */
+module.exports = {
+    mode: 'development',
+    devtool: 'source-map',
+    entry: [
+        './src/index.ts'
+    ],
+    output: {
+        path: __dirname + '/dist',
+        filename: 'index.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'awesome-typescript-loader'
+            }, {
+                test: /\.css$/,
+                exclude: /[\/\\]src[\/\\]/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {loader: 'css-loader'}
+                ]
+            }, {
+                test: /\.css$/,
+                exclude: /[\/\\](node_modules|bower_components|public)[\/\\]/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            importLoaders: 1,
+                            localIdentName: '[path]___[name]__[local]___[hash:base64:5]'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    resolve: { extensions: [".web.ts", ".web.js", ".ts", ".js"] },
+    plugins: [HTMLWebpackPluginConfig, BrowserSyncPluginConfig, ProgressBarPluginConfig]
+}
