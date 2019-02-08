a8b1a7e488297094b642c3f34d0bae7646286100
Jaun1011 <jan@jku.one>
2019-02-08 17:02:35 +0100

    add test for edges II

--- doc/prototype/src/app/graphic/Mapper.spec.ts
+++ doc/prototype/src/app/graphic/Mapper.spec.ts
@@ -6,8 +6,7 @@ import 'mocha';
 
 describe('Edges', () => {
     describe('getEdges()', () => {
-
-        it('should generate edges ', () => {
+        it('should filter edges', () => {
 
             const vert = Mapper.vertices(3, 3);
             const result = Edges.getEdges(vert);
@@ -18,13 +17,23 @@ describe('Edges', () => {
                 {x: 3, y: 0, z: 0},
                 {x: 3, y: 3, z: 0}
             ];
-            console.log(result);
+            expect(expected).to.be.deep.equal(result);
+        });
+
+        it('should filter edges with more values', () => {
+            const vert = Mapper.vertices(5, 3);
+            const result = Edges.getEdges(vert);
+
+            const expected = [
+                {x: 0, y: 0, z: 0},
+                {x: 0, y: 3, z: 0},
+                {x: 3, y: 0, z: 0},
+                {x: 3, y: 3, z: 0}
+            ];
             expect(expected).to.be.deep.equal(result);
         });
     });
 });
-
-
 
 describe('Indices', () => {
     describe('calcIndices()', () => {
